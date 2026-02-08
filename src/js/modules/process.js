51053055e81f680614cbd5c85867fbc89121d288
codezei <mwbox89@gmail.com>
2026-02-08 20:49:45 +0200

    firsy commit

---
+++ src/js/modules/process.js
@@ -0,0 +1,7 @@
+export default function () {
+    let furnitureSlider = new BeerSlider( document.getElementById( "furniture-slider" ) );
+    let mattressSlider = new BeerSlider( document.getElementById( "mattress-slider" ) );
+    let carpetSlider = new BeerSlider( document.getElementById( "carpet-slider" ) );
+    let armchairSlider = new BeerSlider( document.getElementById( "armchair-slider" ) );
+    let pillowSlider = new BeerSlider( document.getElementById( "pillow-slider" ) );
+}
